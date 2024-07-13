eac654e670ceb624ee3604cda9cd3a2ba8911e8d
tamiso132 <tomasxrandi@hotmail.se>
2024-07-13 17:56:40 +0200

    Revert "u"
    This reverts commit 767b5d2d41b227c2fd167296ad6a2df5f99c74f0.

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![feature(inherent_associated_types)]
+
+pub mod App;
+pub mod core;
+pub mod physics;
+pub mod terrain;
+pub mod vulkan;
+
+extern crate ultraviolet as glm;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
