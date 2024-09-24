f3509fb0666b270a71e5f4f2551068a0a34c0e0e
tamiso132 <tomasxrandi@hotmail.se>
2024-09-24 18:24:27 +0200

    asset loader

--- src/core/asset.rs
+++ src/core/asset.rs
@@ -1,23 +1,28 @@
-use std::{collections::HashMap, sync::{mpsc::{self, channel, Receiver, Sender}, Arc, Mutex}};
+use std::{borrow::{Borrow, BorrowMut}, collections::HashMap, sync::{mpsc::{self, channel, Receiver, Sender}, Arc, Mutex}};
 
 use lazy_static::lazy_static;
+
+trait Loader{
+    fn load(file_str: &str) -> PtrWrapper;
+        
+}
+
+struct PtrWrapper(*mut ());
+unsafe impl Sync for PtrWrapper {}
 
 pub struct Queue{
     sender_save: Arc<Mutex<Sender<(String, u32, usize)>>>,
     reciever_save: Receiver<(String, u32, usize)>
 
 }
-}
 impl Queue {
-    pub 
     pub fn get_sender(&self) -> Arc<Mutex<Sender<(String, u32, usize)>>> {
-        self.sender.clone()
+        todo!()
     }
 }
 
 pub struct Asset{
-    is_loading: bool,
-    data: Vec<u8>,
+    data: PtrWrapper,
 }
 
 lazy_static! {
@@ -31,6 +36,10 @@ pub struct AssetLoader{
     is_mutated: Mutex<HashMap<String, bool>>,
 }
 
+unsafe impl Sync for AssetLoader {
+    
+}
+
 
 
 impl AssetLoader {
@@ -42,16 +51,23 @@ impl AssetLoader {
     }
 
     pub fn get<T>(file_name: &str, element_offset: u32, element_size: usize, count: usize){
+        let is_mutated = ASSET_LOADER.is_mutated.lock().unwrap();
+        let val = is_mutated.get(file_name);
+       
+        if val.is_none() || !val.unwrap(){
+            // if it is not being loaded
 
+            // now check if it already has been loaded
+        }
     }
     
-    pub fn load(&self, file_name: &str){
-        let data =  self.cache.get(file_name);
+    pub fn load<T:Loader>(&self, file_name: &str){
+        let mut data =  self.cache.get(file_name);
+        let mut is_mutated_lock = self.is_mutated.lock().unwrap();
         {
-            let mut is_mutated_lock = self.is_mutated.lock().unwrap();
-            let mut is_mutated = is_mutated_lock.get_mut(file_name);
+            let mut is_mutated = is_mutated_lock.get(file_name);
 
-            let mut in_progress = &mut false;
+            let mut in_progress = &false;
 
             if is_mutated.is_some(){
                 in_progress = is_mutated.unwrap();
@@ -61,21 +77,30 @@ impl AssetLoader {
                 // in progress in loading
                 return;
             }
-            else{
-                // start loading
-            }
-
         }
+            data.get_or_insert(&Asset{ data: PtrWrapper(std::ptr::null_mut())}); 
+            is_mutated_lock.insert(file_name.to_owned(), true);
+            
+        let file_name = file_name.to_owned();
+        tasc::blocking::task(|id: tasc::com::WorkerId|{
+            let file_name = file_name;
+            let data_ptr = T::load(&file_name);
+
+            // need to send a complete job
+            AssetLoader::load_complete(file_name, data_ptr);
+        });
       
+    }
+
+    fn load_complete(file_str: String, data_ptr: PtrWrapper){
+        ASSET_LOADER.cache.get(&file_str).insert(&Asset { data: data_ptr });
     }
 
     
 }
 
 
-fn load_file(file_name: &str){
 
-}
 
 
 
